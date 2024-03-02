7926d7f7fcbdea13033bcf708823e39dd3066a44
MohammadBohluli <mohammad.bohluli.8789@gmail.com>
2024-03-03 00:03:19 +0330

    refactor: create button component

--- src/App.jsx
+++ src/App.jsx
@@ -31,12 +31,15 @@ function Friend({ friend }) {
           <p className="text-xs">شما بدهکار هستید</p>
         </div>
       </div>
-      <button className="self-center rounded-2xl bg-purple-400 px-5 py-1 transition-all hover:bg-purple-500">
-        انتخاب
-      </button>
+      <Button>انتخاب</Button>
     </li>
   );
 }
-// function AddFriendForm() {
-//   return <div>فورم وارد کردن دوستان</div>;
-// }
+
+function Button({ children }) {
+  return (
+    <button className="self-center rounded-2xl bg-purple-400 px-5 py-1 transition-all hover:bg-purple-500">
+      {children}
+    </button>
+  );
+}
