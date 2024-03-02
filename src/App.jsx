21968775d45a1a383ee92e5aae58fc56694d55f5
MohammadBohluli <mohammad.bohluli.8789@gmail.com>
2024-03-02 19:42:17 +0330

    initial commit

---
+++ src/App.jsx
@@ -0,0 +1,36 @@
+const initialFriends = [
+  {
+    id: 118836,
+    name: 'محمد',
+    image: 'https://i.pravatar.cc/48?u=118836',
+    balance: -7,
+  },
+  {
+    id: 933372,
+    name: 'سارا',
+    image: 'https://i.pravatar.cc/48?u=933372',
+    balance: 20,
+  },
+  {
+    id: 499476,
+    name: 'علیرضا',
+    image: 'https://i.pravatar.cc/48?u=499476',
+    balance: 0,
+  },
+];
+
+export default function App() {
+  return <div></div>;
+}
+
+// function FreindsList() {
+//   return <div>لیست دوستان</div>;
+// }
+
+// function AddFriendForm() {
+//   return <div>فورم وارد کردن دوستان</div>;
+// }
+
+// function SplitBillForm() {
+//   return <div>فرم پرداختی</div>;
+// }
