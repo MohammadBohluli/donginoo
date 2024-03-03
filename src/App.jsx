d1a09cc3977f670248802aa373e2e18177c54309
MohammadBohluli <mohammad.bohluli.8789@gmail.com>
2024-03-03 12:35:49 +0330

    implement  AddFriendForm component

--- src/App.jsx
+++ src/App.jsx
@@ -3,16 +3,18 @@ import initialFriends from './data';
 
 export default function App() {
   return (
-    <div className="mx-auto my-0 max-w-5xl">
+    <div className="mx-auto my-0 max-w-sm p-2">
       <FreindsList />
+      <AddFreindForm />
+      <Button>بستن</Button>
     </div>
   );
 }
 
 function FreindsList() {
   return (
-    <div>
-      <ul className="mx-auto my-0 max-w-sm p-1">
+    <div className="mb-5">
+      <ul>
         {initialFriends.map((friend) => (
           <Friend friend={friend} key={friend.id} />
         ))}
@@ -23,12 +25,20 @@ function FreindsList() {
 
 function Friend({ friend }) {
   return (
-    <li className="item-center my-1 flex justify-between rounded-md bg-purple-100 p-3">
+    <li className="item-center my-1 flex justify-between rounded-md p-3 hover:bg-purple-100">
       <div className="flex">
         <img className="rounded-full" src={friend.image} alt="" />
         <div className="pr-2">
-          <p className="text-xl">{friend.name}</p>
-          <p className="text-xs">شما بدهکار هستید</p>
+          <h3 className="text-xl">{friend.name}</h3>
+          {friend.balance > 0 && (
+            <p className="text-xs text-green-600">
+              {friend.name} به تو بدهکاره
+            </p>
+          )}
+          {friend.balance < 0 && (
+            <p className="text-xs text-red-600">تو به {friend.name} بدهکاری</p>
+          )}
+          {friend.balance === 0 && <p className="text-xs">هیچکس بدهکار نیست</p>}
         </div>
       </div>
       <Button>انتخاب</Button>
@@ -41,5 +51,18 @@ function Button({ children }) {
     <button className="self-center rounded-2xl bg-purple-400 px-5 py-1 transition-all hover:bg-purple-500">
       {children}
     </button>
+  );
+}
+
+function AddFreindForm() {
+  return (
+    <form className="my-4 flex flex-col gap-1 rounded-md bg-purple-100 p-3">
+      <label>👫 اسم دوستت</label>
+      <input className="rounded-md border-2 border-solid pr-2" type="text" />
+
+      <label>🖼 آدرس عکس</label>
+      <input className="rounded-md border-2 border-solid pr-2" type="text" />
+      <Button>اضافه کردن</Button>
+    </form>
   );
 }
